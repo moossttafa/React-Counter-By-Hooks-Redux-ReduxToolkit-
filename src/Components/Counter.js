3bd1759512b8c08941dd7b41c44f3bd9d036228b
moossttafa <mostafo7102000@gmail.com>
2022-03-08 14:58:54 +0200

    create counter by hooks (useState)

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from "./Counter.module.css"
 
 const Counter = () => {
+  const [count, setCount] = useState(0)
 
+  const incrementHandler = () => {
+    setCount(count => count + 1)
+  }
+  const decrementHandler = () => {
+    setCount(count => count - 1)
+  }
+  const toggleCounterHandler = () =>{
+    setCount(0)
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Counter By Hooks</h1>
-      <div className={classes.value}>value</div>
+      <div className={classes.value}>{count}</div>
       <div>
-        <button >Increment</button>
-        <button >Decrement</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
-      <button>Reset</button>
+      <button onClick={toggleCounterHandler}>Reset</button>
     </main>
   )
 }

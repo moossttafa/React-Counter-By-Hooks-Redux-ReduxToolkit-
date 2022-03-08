ca104194bfe1f741e9b1f8bb758f937911fa25a9
moossttafa <mostafo7102000@gmail.com>
2022-03-08 15:52:14 +0200

    counter with redux

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,28 +1,31 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import classes from "./Counter.module.css"
 
-const Counter = () => {
-  const [count, setCount] = useState(0)
+const Counter = () => { 
 
-  const incrementHandler = () => {
-    setCount(count => count + 1)
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.count)
+
+  const incrementHandler = () => { 
+    dispatch({type : "increment" , payload : 1})
   }
-  const decrementHandler = () => {
-    setCount(count => count - 1)
+  const decrementHandler = () => { 
+    dispatch({type : "decrement" , payload : 1})
   }
-  const toggleCounterHandler = () =>{
-    setCount(0)
+  const ReserHandler = () =>{ 
+    dispatch({type : "reset" , payload : 0})
   }
 
   return (
     <main className={classes.counter}>
-      <h1>Counter By Hooks</h1>
-      <div className={classes.value}>{count}</div>
+      <h1>Counter By Redux</h1>
+      <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
-      <button onClick={toggleCounterHandler}>Reset</button>
+      <button onClick={ReserHandler}>Reset</button>
     </main>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'; 
+import { Provider } from 'react-redux';
+import Store from './Store';
 
 ReactDOM.render(
+  <Provider store={Store}> 
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 ); 
